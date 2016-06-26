c793ea7c8156c7cf4f6784c6b21d114f10952360
tony <qingfengmiaokang@qingfengmiaokangdeMacBook-Pro.local>
2016-06-26 17:34:42 +0800

    add

--- renderDoc.js
+++ renderDoc.js
@@ -6,7 +6,7 @@ var config = {
       'reference':'参考文档',
       '3_API':'接口文档',
       '4_support':'技术支持',
-      'join-guied':'接入指南'
+      '1_join_guied':'接入指南'
     }
 };
 var fs = require('fs'),
