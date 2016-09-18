6a7e3cf4e3b7881e7b08a5bf43b62a726219ac71
harttle <yangjvn@126.com>
2016-09-18 15:04:59 +0800

    fix: describe underscore test

--- test/utils/underscore.js
+++ test/utils/underscore.js
@@ -4,55 +4,58 @@
  */
 
 define(['src/utils/underscore'], function(_) {
-    var obj = {
-        foo: 'bar',
-        bar: false
-    };
-    var arr = ['foo', 'bar'];
-    function func(a, b, c) {
-        return a + b + c;
-    }
-    describe('.keysIn()', function() {
-        it('should return array of keys', function() {
-            expect(_.keysIn(obj)).to.deep.equal(['foo', 'bar']);
+    describe('underscore', function() {
+        var obj = {
+            foo: 'bar',
+            bar: false
+        };
+        var arr = ['foo', 'bar'];
+
+        function func(a, b, c) {
+            return a + b + c;
+        }
+        describe('.keysIn()', function() {
+            it('should return array of keys', function() {
+                expect(_.keysIn(obj)).to.deep.equal(['foo', 'bar']);
+            });
         });
-    });
-    describe('.isEmpty()', function() {
-        it('should return true for empty array', function() {
-            expect(_.isEmpty([])).to.equal(true);
+        describe('.isEmpty()', function() {
+            it('should return true for empty array', function() {
+                expect(_.isEmpty([])).to.equal(true);
+            });
+            it('should return true for empty string', function() {
+                expect(_.isEmpty('')).to.equal(true);
+            });
         });
-        it('should return true for empty string', function() {
-            expect(_.isEmpty('')).to.equal(true);
+        describe('.split()', function() {
+            it('should support string separator', function() {
+                expect(_.split('abcb', 'b')).to.deep.equal(['a', 'c', '']);
+            });
+            it('should support RegExp separator', function() {
+                expect(_.split('abcb', /b/)).to.deep.equal(['a', 'c', '']);
+            });
         });
-    });
-    describe('.split()', function() {
-        it('should support string separator', function() {
-            expect(_.split('abcb', 'b')).to.deep.equal(['a', 'c', '']);
+        describe('.partialRight()', function() {
+            it('should support zero partials', function() {
+                var g = _.partialRight(func);
+                expect(g('a', 'b', 'c')).to.equal('abc');
+            });
+            it('should support one partials', function() {
+                var g = _.partialRight(func, 'c');
+                expect(g('a', 'b')).to.equal('abc');
+            });
+            it('should support two partials', function() {
+                var g = _.partialRight(func, 'b', 'c');
+                expect(g('a')).to.equal('abc');
+            });
         });
-        it('should support RegExp separator', function() {
-            expect(_.split('abcb', /b/)).to.deep.equal(['a', 'c', '']);
-        });
-    });
-    describe('.partialRight()', function() {
-        it('should support zero partials', function() {
-            var g = _.partialRight(func);
-            expect(g('a', 'b', 'c')).to.equal('abc');
-        });
-        it('should support one partials', function() {
-            var g = _.partialRight(func, 'c');
-            expect(g('a', 'b')).to.equal('abc');
-        });
-        it('should support two partials', function() {
-            var g = _.partialRight(func, 'b', 'c');
-            expect(g('a')).to.equal('abc');
-        });
-    });
-    describe('.format()', function(){
-        it('should support multiple args', function(){
-            expect(_.format('foo%sbar%s', 'a', 'b')).to.equal('fooabarb');
-        });
-        it('should support plain string', function(){
-            expect(_.format('foo%sbar%s')).to.equal('foo%sbar%s');
+        describe('.format()', function() {
+            it('should support multiple args', function() {
+                expect(_.format('foo%sbar%s', 'a', 'b')).to.equal('fooabarb');
+            });
+            it('should support plain string', function() {
+                expect(_.format('foo%sbar%s')).to.equal('foo%sbar%s');
+            });
         });
     });
 });
