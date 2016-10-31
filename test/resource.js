f007df2050caeee8368413c8577c2147969403b7
harttle <yangjvn@126.com>
2016-10-31 20:59:27 +0800

    fix: #then() didn't return a new Promise instance

--- test/resource.js
+++ test/resource.js
@@ -42,46 +42,43 @@ define(['../src/resource'], function(Resource) {
 
         describe('resource', function() {
             var Cat = new Resource('http://json.com/person/:pid/cat/:id');
-            it('should create a RESTful item', function(done) {
+            it('should create a RESTful item', function() {
                 var cat = {
                     name: 'harttle'
                 };
                 var query = {
                     pid: 1
                 };
-                Cat.create(cat, query).then(function(cats, status, xhr) {
+                return Cat.create(cat, query).then(function(xhr) {
                     expect(xhr.url).to.equal('http://json.com/person/1/cat/');
                     expect(xhr.method).to.equal('POST');
                     expect(xhr.requestBody).to.equal('name=harttle');
-                    done();
-                }).catch(done);
+                });
             });
-            it('should query a RESTful item', function(done) {
+            it('should query a RESTful item', function() {
                 var cat = {
                     pid: 1,
                     id: 2
                 };
-                Cat.query(cat).then(function(cat, status, xhr) {
+                return Cat.query(cat).then(function(xhr) {
                     expect(xhr.url).to.equal('http://json.com/person/1/cat/2');
                     expect(xhr.method).to.equal('GET');
-                    done();
-                }).catch(done);
+                });
             });
-            it('should query RESTful items', function(done) {
+            it('should query RESTful items', function() {
                 var cat = {
                     pid: 1
                 };
-                Cat.query(cat).then(function(cats, status, xhr) {
+                return Cat.query(cat).then(function(xhr) {
                     expect(xhr.url).to.equal('http://json.com/person/1/cat/');
                     expect(xhr.method).to.equal('GET');
-                    expect(cats).to.deep.equal([{
+                    expect(xhr.data).to.deep.equal([{
                         id: 2,
                         name: 'harttle'
                     }]);
-                    done();
-                }).catch(done);
+                });
             });
-            it('should update a RESTful item', function(done) {
+            it('should update a RESTful item', function() {
                 var cat = {
                     name: 'harttle'
                 };
@@ -89,23 +86,21 @@ define(['../src/resource'], function(Resource) {
                     pid: 1,
                     id: 2
                 };
-                Cat.update(cat, query).then(function(cats, status, xhr) {
+                return Cat.update(cat, query).then(function(xhr) {
                     expect(xhr.url).to.equal('http://json.com/person/1/cat/2');
                     expect(xhr.method).to.equal('PUT');
                     expect(xhr.requestBody).to.contain('name=harttle');
-                    done();
-                }).catch(done);
+                });
             });
-            it('should delete a RESTful item', function(done) {
+            it('should delete a RESTful item', function() {
                 var query = {
                     pid: 1,
                     id: 2
                 };
-                Cat.delete(query).then(function(cats, status, xhr) {
+                return Cat.delete(query).then(function(xhr) {
                     expect(xhr.url).to.equal('http://json.com/person/1/cat/2');
                     expect(xhr.method).to.equal('DELETE');
-                    done();
-                }).catch(done);
+                });
             });
         });
     });
