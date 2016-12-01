0d4f4431d993329d9175d168330f5b3c7beab2c1
oott123 <git@public.oott123.com>
2016-12-01 13:53:33 +0800

    feature: _.wrap

--- test/utils/underscore.js
+++ test/utils/underscore.js
@@ -201,5 +201,28 @@ define(['src/utils/underscore'], function(_) {
                 expect(_.format('foo%sbar%s')).to.equal('foo%sbar%s');
             });
         });
+        describe('.wrap', function () {
+            it('should return a function', function () {
+                expect(_.wrap(undefined, func)).to.be.a('function');
+            });
+            it('should pass arguments', function () {
+                var wrapper = sinon.spy();
+                _.wrap(obj, wrapper)(1, 2, 3);
+                expect(wrapper).to.be.calledWith(obj, 1, 2, 3);
+            });
+            it('should pass context', function () {
+                var wrapper = sinon.spy();
+                var context = {};
+                _.wrap(obj, wrapper).call(context);
+                expect(wrapper).to.be.calledOn(context);
+            });
+            it('should pass return value', function () {
+                var three = _.wrap(1, func)(0, 2);
+                expect(three).to.equal(3);
+            });
+            it('should throws unless passing a function as wrapper', function () {
+                expect(_.wrap.bind(this, undefined, obj)).to.throw(/wrapper should be a function/);
+            });
+        });
     });
 });
