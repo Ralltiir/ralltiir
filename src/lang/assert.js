6aa93e9f74ee6bebabe6442605c275c17043822b
harttle <yangjvn@126.com>
2017-04-06 11:10:31 +0800

    fix fecs for assert/promise/map/main

--- src/lang/assert.js
+++ src/lang/assert.js
@@ -1,9 +1,13 @@
-define(function() {
-    function assert(predict, msg){
-        if(!predict){
+/**
+ * @file assert.js assert utility
+ * @author harttle<yangjun14@baidu.com>
+ */
+
+define(function () {
+    function assert(predict, msg) {
+        if (!predict) {
             throw new Error(msg);
         }
     }
     return assert;
 });
-
