17ca49c0dbc678a7bbe3e93d19c705a23020b0ed
harttle <yangjvn@126.com>
2017-04-06 18:35:43 +0800

    update coverage CI

--- karma.conf.js
+++ karma.conf.js
@@ -1,9 +1,9 @@
-/*
+/**
  * @author yangjun14(yangjun14@baidu.com)
  * @file Karma 配置文件
  */
 
-module.exports = function(config) {
+module.exports = function (config) {
     config.set({
 
         // base path that will be used to resolve all patterns (eg. files, exclude)
@@ -14,7 +14,6 @@ module.exports = function(config) {
         // Important: 下列数组中文件将『逆序载入』
         frameworks: ['mocha', 'chai-as-promised', 'chai-sinon', 'chai'],
 
-
         // list of files / patterns to load in the browser
         files: [
             'lib/esl.js',
@@ -24,13 +23,11 @@ module.exports = function(config) {
             }, {
                 pattern: 'test/**/*.js',
                 included: false
-            },
+            }
         ],
-
 
         // list of files to exclude
         exclude: [],
-
 
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
@@ -41,7 +38,6 @@ module.exports = function(config) {
             'src/**/*.js': ['coverage']
         },
 
-
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
@@ -50,33 +46,34 @@ module.exports = function(config) {
             outputDir: 'build/test-reports/result'
         },
         coverageReporter: {
-            dir: 'test-reports/coverage'  // relative to basePath
+            reporters: [
+                {
+                    type: 'html',
+                    dir: 'test-reports/coverage' // relative to basePath
+                }, {
+                    type: 'text-summary'
+                }
+            ]
         },
-
 
         // web server port
         port: 9876,
 
-
         // enable / disable colors in the output (reporters and logs)
         colors: true,
-
 
         // level of logging
         // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
         // Note: 如果要调试Karma，请设置为DEBUG
         logLevel: config.LOG_INFO,
 
-
         // enable / disable watching file and executing tests whenever any file changes
         // Note: 代码改动自动运行测试，需要singleRun为false
         autoWatch: false,
 
-
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         browsers: [process.env.BROWSER || 'Chrome'],
-
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
